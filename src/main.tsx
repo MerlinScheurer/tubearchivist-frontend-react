0011a8a1299ba37b570a3a39d74d1ca4a9d5a0ab
MerlinScheurer <merlin.scheurer@Yahoo.de>
2024-05-12 13:35:41 +0200

    Add userConfig to Playlist route

--- src/main.tsx
+++ src/main.tsx
@@ -186,7 +186,9 @@ const router = createBrowserRouter(
               return redirect(Routes.Login);
             }
 
-            return {};
+            const userConfig = await loadUserConfig();
+
+            return { userConfig };
           },
         },
         {
